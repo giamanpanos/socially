cec9c81bc31d194ac84853902dc5740a3102d4f8
giamanpanos <90336670+giamanpanos@users.noreply.github.com>
2025-01-20 00:43:13 +0200

    project completed

---
+++ src/app/tasks/page.tsx
@@ -0,0 +1,10 @@
+async function TaskPage() {
+  const response = await fetch("http://localhost:3000/api/tasks");
+  const tasks = await response.json();
+
+  console.log(tasks);
+
+  return <div>TaskPage</div>;
+}
+
+export default TaskPage;
